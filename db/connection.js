b91c6d6edcad023440067d56b0ffb5d1022ae76a
Bryan Pero <bryanpero@gmail.com>
2020-06-20 16:24:54 -0400

    added connection.js & dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
-// require mysql
+const mysql = require("mysql");
+require("dotenv").config();
 
-const connection = mysql.creatConnection({
-  //host-local host
-  //user:
-  //pasword
-  //database name
+const connection = mysql.createConnection({
+  multipleStatements: true,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-connection.connect();
+connection.connect((err) => {
+  if (err) throw err;
+  // console.log("connection made to database");
+});
 
 module.exports = connection;
